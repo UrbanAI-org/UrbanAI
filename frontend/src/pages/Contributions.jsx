0adf8219e2c873f5f3b31ca1416667876191f65f
Bob He <z5366732@ad.unsw.edu.au>
2023-03-22 17:09:28 +1100

    periodic commit

---
+++ frontend/src/pages/Contributions.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const Contributions = () => {
+    return (
+        <div>
+            <NavBar />
+            The following individuals have contributed towards this project:
+        </div>
+
+    )
+}
+
+export default Contributions;

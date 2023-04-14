234bc42e7401786642d9a18bb0f87a093f9523a4
Alice <alicewan0606@gmail.com>
2023-04-14 22:52:32 +1000

    added gradient shading

--- frontend/src/components/LandscapeScene/LandscapeScene.jsx
+++ frontend/src/components/LandscapeScene/LandscapeScene.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Region from "./Region";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import DatGui, { DatNumber } from "react-dat-gui";
 import "react-dat-gui/dist/index.css";
 import "./LandscapeScene.css";
+import * as THREE from 'three';
 
 const LandscapeScene = ({ responseBody }) => {
-  const [position, setPosition] = useState([0, 0, 0]);
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0, rotationX: 0, rotationY: 0, rotationZ: 0 });
+  const [lookAt, setLookAt] = useState([0, 0, 0]);
+
+  const orbitControlsRef = useRef();
 
   if (responseBody === null) {
     return (
       <Canvas className="landscape-scene">
-        {/*...your existing code*/}
       </Canvas>
     );
   } else {
     return (
       <div className="landscape-scene">
-        <DatGui data={position} onUpdate={setPosition}>
-          <DatNumber path="0" label="X" min={-100} max={100} step={0.1} />
-          <DatNumber path="1" label="Y" min={-100} max={100} step={0.1} />
-          <DatNumber path="2" label="Z" min={-100} max={100} step={0.1} />
+        <DatGui data={position} onUpdate={setPosition} >
+          <DatNumber path="x" label="X" min={-300} max={300} step={0.1} />
+          <DatNumber path="y" label="Y" min={-300} max={300} step={0.1} />
+          <DatNumber path="z" label="Z" min={-300} max={300} step={0.1} />
+          <DatNumber path="rotationX" label="Rotation X" min={-Math.PI} max={Math.PI} step={0.01} />
+          <DatNumber path="rotationY" label="Rotation Y" min={-Math.PI} max={Math.PI} step={0.01} />
+          <DatNumber path="rotationZ" label="Rotation Z" min={-Math.PI} max={Math.PI} step={0.01} />
         </DatGui>
         <Canvas >
-          <Region position={position} />
-          <OrbitControls />
+          <Region position={position} setLookAt={setLookAt}/>
+          <OrbitControls ref={orbitControlsRef} target={new THREE.Vector3(lookAt[0], lookAt[1], lookAt[2])} />
         </Canvas>
       </div>
     );
@@ -33,34 +39,3 @@ const LandscapeScene = ({ responseBody }) => {
 };
 
 export default LandscapeScene;
-
-
-
-
-// import React from "react";
-// import Region from "./Region";
-// import { Canvas } from '@react-three/fiber'
-// import { OrbitControls } from '@react-three/drei'
-// import './LandscapeScene.css';
-
-// const LandscapeScene = ({responseBody}) => {
-// 	if (responseBody === null) {
-// 		return(
-// 			<Canvas className="landscape-scene">
-// 			</Canvas>
-// 		)
-// 	} else {
-// 		return (
-// 			<Canvas className="landscape-scene">
-// 				<Region position={[0, 0 , 0]} />
-// 				<mesh>
-// 					<planeBufferGeometry attach="geometry" args={[25, 15]} />
-// 					<meshPhongMaterial attach="material" color="green" />
-// 				 </mesh>
-// 				<OrbitControls />
-// 			</Canvas>
-// 		)
-// 	}
-// }
-
-// export default LandscapeScene;
--- frontend/src/components/LandscapeScene/Region.jsx
+++ frontend/src/components/LandscapeScene/Region.jsx
@@ -1,26 +1,62 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { PLYLoader } from 'three-stdlib'
+import { PLYLoader } from 'three-stdlib';
+import { useThree } from '@react-three/fiber';
 
-const Region = ({ position }) => {
-  const [geo, setGeo] = useState(new THREE.BoxGeometry());
+// Custom vertex shader
+const vertexShader = `
+  varying vec3 vPosition;
 
-  const attr = geo.attributes;
+  void main() {
+    vPosition = position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+// Custom fragment shader
+const fragmentShader = `
+  varying vec3 vPosition;
+  uniform float minHeight;
+  uniform float maxHeight;
+
+  void main() {
+    // Calculate normalized height value (0 to 1)
+    float height = (vPosition.y - minHeight) / (maxHeight - minHeight);
+    // Use height value to interpolate gradient color
+    vec3 color = mix(vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), height);
+    gl_FragColor = vec4(color, 1.0);
+  }
+`;
+
+const Region = ({ position, setLookAt }) => {
+  const [geo, setGeo] = useState(null);
+  const [minHeight, setMinHeight] = useState(Number.POSITIVE_INFINITY);
+  const [maxHeight, setMaxHeight] = useState(Number.NEGATIVE_INFINITY);
+  const camera = useThree((state) => state.camera); // Access camera object
+
+  // Create a ref to store the mesh material
+  const materialRef = useRef();
+
   useEffect(() => {
     const loader = new PLYLoader();
     loader.load(
       "test.ply",
       function (geometry) {
-        console.log(geometry)
-        console.log(geometry.boundingSphere.center)
-        console.log(geometry.attributes.position.array)
+        setLookAt([
+          geometry.boundingSphere.center.y,
+          geometry.boundingSphere.center.x,
+          geometry.boundingSphere.center.z
+        ]);
+
         setGeo(geometry);
+
+        // Set far property of the camera
+        camera.far = 1000000; // Set a large value for far property
+        camera.updateProjectionMatrix(); // Apply changes to camera
       },
-      // called when loading is in progress
       function (xhr) {
         console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
       },
-      // called when loading has errors
       function (error) {
         console.log("An error happened");
         console.log(error);
@@ -28,14 +64,57 @@ const Region = ({ position }) => {
     );
   }, []);
 
+  // Calculate min and max heights
+  useEffect(() => {
+    if (geo) {
+      const { min, max } = calculateMinMaxHeights();
+      setMinHeight(min);
+      setMaxHeight(max);
+    }
+  }, [geo]);
+
+  // Function to calculate min and max heights
+  const calculateMinMaxHeights = () => {
+    const positions = geo.attributes.position.array;
+    let min = Number.POSITIVE_INFINITY;
+    let max = Number.NEGATIVE_INFINITY;
+    for (let i = 1; i < positions.length; i += 3) {
+      const height = positions[i];
+      min = Math.min(min, height);
+      max = Math.max(max, height);
+    }
+
+    return { min, max };
+  };
+
+  // Update the uniform values in the material
+  useEffect(() => {
+    if (materialRef.current) {
+      materialRef.current.uniforms.minHeight.value = minHeight;
+      materialRef.current.uniforms.maxHeight.value = maxHeight;
+    }
+  }, [minHeight, maxHeight]);
+
   return (
     <mesh
-      position={position}
-      // ref={ref}
-      geometry={geo}>
-      <meshBasicMaterial color={'lime'} wireframe />
+      position={[position.x, position.y, position.z]}
+      geometry={geo}
+      onCreated={(mesh) => {
+        // Store the material in the ref
+        materialRef.current = mesh.material;
+      }}
+    >
+      <shaderMaterial
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        wireframe
+        uniforms={{
+          minHeight: { value: minHeight },
+          maxHeight: { value: maxHeight },
+        }}
+      />
     </mesh>
-  )
-}
+  );
+};
 
 export default Region;
